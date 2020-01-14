78feb077a10b2d791a82cc9ae0a4d6630a9a0689
robchun <robchunwong@gmail.com>
2020-01-14 11:18:31 +0800

    test

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, url_for, request
-from app import app
+from app import app, db
 from app.forms import LoginForm, AForm
 from app.models import User, Post
 
--- config.py
+++ config.py
@@ -5,4 +5,4 @@ class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://onion:onion1234@10.80.200.35:3306/beast'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	SQLALCHEMY_ECHO = True
+	SQLALCHEMY_ECHO = False
